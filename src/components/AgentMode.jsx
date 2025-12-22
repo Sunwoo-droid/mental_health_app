32b7a619092b5714129dbf1da7a79e179586dd06
Sunwoo-droid <sunwoo.f.choi@gmail.com>
2025-12-22 15:37:20 -0600

    Netlify Errors Fixed

--- src/components/AgentMode.jsx
+++ src/components/AgentMode.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { ArrowRightIcon, ArrowPathIcon, BoltIcon, ChartBarIcon, AcademicCapIcon, LightBulbIcon } from '@heroicons/react/24/outline';
 import { storyNodes } from './StoryNodes';
 
